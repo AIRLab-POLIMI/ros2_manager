5d557dd54593c53593680e1d6d58269e4aaa8a99
leonardogargani <leonardo.gargani@mail.polimi.it>
2025-06-03 17:31:12 +0200

    Enable NVIDIA GPU rendering (for increased Gazebo performance)

--- docker/ros2_base/entrypoint.sh
+++ docker/ros2_base/entrypoint.sh
@@ -4,10 +4,22 @@
 # Exit script on any error
 set -e
 
-# One-timme commands
-if [ ! -f /tmp/initialized ]; then
+# One-time commands
+if [ ! -f /tmp/ros2_base/initialized ]; then
+
     # Source ROS 2 installation
     echo "source /opt/ros/${ROS2_DISTRO}/local_setup.bash" >> /home/${USER_NAME}/.bashrc
+
+    # Enable ROS 2 colorized logging
+    echo "export RCUTILS_COLORIZED_OUTPUT=1" >> /home/${USER_NAME}/.bashrc
+
+    # Use NVIDIA GPU for rendering
+    echo "export __NV_PRIME_RENDER_OFFLOAD=1" >> /home/${USER_NAME}/.bashrc
+    echo "export __GLX_VENDOR_LIBRARY_NAME=nvidia" >> /home/${USER_NAME}/.bashrc
+
+    # Create a flag file to indicate that this script has been already run
+    touch /tmp/ros2_base/initialized
+
 fi
 
 # Open an interactive bash shell
--- docker/ros2_workspaces/entrypoint.sh
+++ docker/ros2_workspaces/entrypoint.sh
@@ -4,16 +4,15 @@
 # Exit script on any error
 set -e
 
-# One-timme commands
-if [ ! -f /tmp/initialized ]; then
-    # Source ROS 2 installation
-    echo "source /opt/ros/${ROS2_DISTRO}/local_setup.bash" >> /home/${USER_NAME}/.bashrc
-    # Source the ROS 2 workspace
+# One-time commands
+if [ ! -f /tmp/ros2_workspaces/initialized ]; then
+    
+    # Source ROS 2 workspace
     echo "source ${ROS2_WS_PATH}/install/setup.bash" >> /home/${USER_NAME}/.bashrc
-    # Enable ROS colorized logging
-    echo "export RCUTILS_COLORIZED_OUTPUT=1" >> /home/${USER_NAME}/.bashrc
+    
     # Create a flag file to indicate that this script has been already run
-    touch /tmp/initialized
+    touch /tmp/ros2_workspaces/initialized
+
 fi
 
 # Open an interactive bash shell
